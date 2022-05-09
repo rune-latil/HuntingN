01528fb91ee77c3c579c439431cc6a83fe6fad11
ssimplay <ssimplay@naver.com>
2022-05-09 12:16:06 +0900

    start button update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
+const main = document.querySelector("#main");
+const qna = document.querySelector('#qna')
+const ending = document.querySelector('#ending')
+
+
 function start() {
+    main.style.webkitAnimation = 'fadeOut 1s';
+    main.style.Animation = 'fadeOut 1s';
+
+    setTimeout(function () {
+        qna.style.webkitAnimation = 'fadeIn 1s';
+        qna.style.Animation = 'fadeIn 1s';
+        setTimeout(function () {
+            main.style.display = 'none';
+            qna.style.display = 'block';
+        }, 600)
+    })
 
 }
