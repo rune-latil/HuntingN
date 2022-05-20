160717288bfa7df2b8a018ff85f7770e7f0d2ff6
ssimplay <ssimplay@naver.com>
2022-05-20 23:03:37 +0900

    Update ShareTwitter

--- js/main.js
+++ js/main.js
@@ -109,15 +109,13 @@ function start() {
 
 }
 
-function setShare() {
+
+/* 트위터 공유 */
+function shareTwitter() {
     let resultImg = document.querySelector('.endingImg');
     let resultAlt = resultImg.firstElementChild.alt;
 
-    /* 트위터 공유 */
-    function shareTwitter() {
-        let sendText = "니사의 사냥대회 Ending"; // 전달할 텍스트
-        let sendUrl = "https://wefershuntingn.netlify.app/page/ending"; // 전달할 URL
-        window.open("https://twitter.com/intent/tweet?text=" + sendText + "&url=" + sendUrl + resultAlt + '.html');
-    }
+    let sendText = "니사의 사냥대회 Ending"; // 전달할 텍스트
+    let sendUrl = "https://wefershuntingn.netlify.app/page/ending"; // 전달할 URL
+    window.open("https://twitter.com/intent/tweet?text=" + sendText + "&url=" + sendUrl + resultAlt + '.html');
 }
-
